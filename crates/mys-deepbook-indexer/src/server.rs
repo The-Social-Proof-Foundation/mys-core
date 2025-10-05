1ab8193e5ebf9248d9854cbc239286d37b8af10b
Brandon Shaw <unobrandon@mac.com>
2025-10-05 02:31:58 -0500

    minor

--- crates/mys-deepbook-indexer/src/server.rs
+++ crates/mys-deepbook-indexer/src/server.rs
@@ -40,7 +40,7 @@ use mys_types::{
 };
 use tokio::join;
 
-pub const MYS_MAINNET_URL: &str = "https://fullnode.mainnet.mysocial.network:443";
+pub const MYS_MAINNET_URL: &str = "http://fullnode.testnet.mysocial.network:9000";
 pub const GET_POOLS_PATH: &str = "/get_pools";
 pub const GET_HISTORICAL_VOLUME_BY_BALANCE_MANAGER_ID_WITH_INTERVAL: &str =
     "/historical_volume_by_balance_manager_id_with_interval/:pool_names/:balance_manager_id";
